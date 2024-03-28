ff8ca997497e9574e0617004ba20b5b55ca8244c
dayrons <dayronstovar@gmail.com>
2024-03-28 13:38:27 -0400

    Modificacion de seeders

--- lib/databases/seeders/canchas_seeder.dart
+++ lib/databases/seeders/canchas_seeder.dart
@@ -8,12 +8,12 @@ import 'package:app_tenis/models/cancha.dart';
         'descripcion': "Bello ambiente y siempre soleado",
         'imagen': "assets/cancha_A.jpg",
         'lat': "10",
-        'precio': 50.00,
+        'precio': "50.00",
         'long': "10",
       },
       {
         'nombre': "Prados del este",
-        'precio': 350.00,
+        'precio': "350.00",
         'direccion': "direcion",
         'descripcion': "Hermosas canchas rodeadas de naturaleza",
         'imagen': "assets/cancha_B.jpg",
@@ -22,7 +22,7 @@ import 'package:app_tenis/models/cancha.dart';
       },
       {
         'nombre': "El bosque",
-        'precio': 180.00,
+        'precio': "180.00",
         'direccion': "direcion",
         'descripcion': "Canchas con vista a un hermoso bosque",
         'imagen': "assets/cancha_C.jpg",
